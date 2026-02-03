8f4dbc9908f3975bb1f86bddbba1272e57584051
TJClarke58 <nimblegorilla58@gmail.com>
2026-02-03 10:32:36 -0500

    Completed discussion questions and model testing

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -1,6 +1,6 @@
 # packages
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier 
 
 # set seed
 seed = 314
@@ -12,7 +12,7 @@ def train_model(X, y, seed=seed):
     model = GradientBoostingClassifier(
         learning_rate=0.1,
         n_estimators=100,
-        max_depth=8,
+        max_depth=63,
         subsample=1,
         min_samples_leaf=1,
         random_state=seed
